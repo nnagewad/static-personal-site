250d8b3efbf2c066848389bf06ae53e6cf311bd6
Nikin Nagewadia <nikin.n@gmail.com>
2021-11-27 14:44:23 +0000

    Update class names for font-sizes

--- src/filters/update-tags.js
+++ src/filters/update-tags.js
@@ -2,9 +2,9 @@ module.exports = (post) => {
   const updatedCopy = post
     .replace(/(<h4>)+?/, "<p class='lead-paragraph'>")
     .replace(/(<\/h4>)+?/, "</p>")
-    .replace(/(<h3>)/g, "<h2 class='headline-m'>")
+    .replace(/(<h3>)/g, "<h2 class='font-size-2'>")
     .replace(/(<\/h3>)/g, "</h2>")
-    .replace(/(<h4>)/g, "<h3 class='headline-sm'>")
+    .replace(/(<h4>)/g, "<h3 class='font-size-1'>")
     .replace(/(<\/h4>)/g, "</h3>")
     .replace(/(<img src=.https:\/\/medium.*>)/g, "");
   return updatedCopy;

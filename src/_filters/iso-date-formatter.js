a7a456135f9ea611b8bdee175c3b90cc5ae46a38
nnagewad <nikin.n@gmail.com>
2025-01-20 21:19:09 +0000

    Create and iso date filter

---
+++ src/_filters/iso-date-formatter.js
@@ -0,0 +1,18 @@
+module.exports = value => {
+  let month = [
+    "01",
+    "02",
+    "03",
+    "04",
+    "05",
+    "06",
+    "07",
+    "08",
+    "09",
+    "10",
+    "11",
+    "12"
+  ];
+  let date = new Date(value);
+  return `${date.getUTCFullYear()}-${month[date.getMonth()]}-${(date.getDate())}`;
+}

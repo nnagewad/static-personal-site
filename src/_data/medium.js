8330324981bb770b6909d34fa3afed881d1a55a0
nnagewad <nikin.n@gmail.com>
2025-02-07 18:24:08 +0000

    Hide and build

--- src/_data/medium.js
+++ src/_data/medium.js
@@ -1,7 +1,8 @@
 import Cache from '@11ty/eleventy-fetch';
+import API_KEY from './api.js';
 
 export default async function() {
-  let url = "https://v1.nocodeapi.com/nikin/medium/jiEUjZNwxwIIzlUq";
+  let url = `https://v1.nocodeapi.com/nikin/medium/${API_KEY}`;
   return Cache(url, {
     duration: "1d", 
     type: "json"

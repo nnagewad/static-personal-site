5876bbd0fe3b49829be3e4ad34f0249a34d4e286
nnagewad <nikin.n@gmail.com>
2025-03-26 11:30:06 +0000

    Use RSS to JSON instead of NoCodeApi

--- src/_data/medium.js
+++ src/_data/medium.js
@@ -2,9 +2,19 @@ import Cache from '@11ty/eleventy-fetch';
 import API_KEY from './api.js';
 
 export default async function() {
-  let url = `https://v1.nocodeapi.com/nikin/medium/${API_KEY}`;
-  return Cache(url, {
-    duration: "1d", 
-    type: "json"
-  });
-}
+  let mediumUsername = "nkngwd"; // Replace with your username
+  let mediumUrl = `https://medium.com/feed/@${mediumUsername}`;
+  let apiUrl = `https://api.rss2json.com/v1/api.json?rss_url=${encodeURIComponent(mediumUrl)}&api_key=${API_KEY}`;
+
+  try {
+    let jsonData = await Cache(apiUrl, {
+        duration: "1d", // Cache for 1 day
+        type: "json",
+    });
+
+    return jsonData.items || []; // Return the array of posts
+  } catch (error) {
+    console.error("Error fetching Medium RSS JSON:", error);
+    return [];
+  }
+};

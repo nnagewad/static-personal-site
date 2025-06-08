27512b32eef151cc690d3e89e6910c1c9b66344c
nnagewad <nikin.n@gmail.com>
2025-06-08 19:23:58 +0100

    Remove custom filter

--- src/_filters/update-for-xml.js
+++
@@ -1,9 +0,0 @@
-export default (post) => {
-  const updatedForXML = post
-    .replace(/&(?!amp;|lt;|gt;|quot;|#39;)/g, "&amp;")
-    // Ensure self-closing tags
-    .replace(/<img([^>]*?)(?<!\/)>/g, "<img$1 />")
-    .replace(/<source([^>]*?)(?<!\/)>/g, "<source$1 />")
-    .replace(/<br([^>]*?)(?<!\/)>/g, "<br$1 />");
-  return updatedForXML;
-};
